96b8e83d65e1fabb31c7e2ec299f8a6419914635
hamadatomoki <hamada.tomoki01@gmail.com>
2023-03-22 11:58:35 +0900

    :sparkles: feat: add pulldown-cmark-to-cmark example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate pulldown_cmark;
+extern crate pulldown_cmark_to_cmark;
+pub use pulldown_cmark::Parser;
+pub use pulldown_cmark_to_cmark::cmark;
+pub use std::env;
+pub use std::ffi::OsString;
+pub use std::fs::File;
+pub use std::io::stdout;
+pub use std::io::{Read, Write};
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +14,28 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        fn main() {
+            let path = env::args_os()
+                .skip(1)
+                .next()
+                .expect("First argument is markdown file to display");
+
+            let md = read_to_string(path);
+            let mut buf = String::with_capacity(md.len() + 128);
+            cmark(
+                Parser::new_ext(&md, pulldown_cmark::Options::all()),
+                &mut buf,
+                None,
+            )
+            .unwrap();
+            stdout().write_all(buf.as_bytes()).unwrap();
+        }
+
+        fn read_to_string(path: OsString) -> String {
+            let mut file = File::open(&path).expect("file to exist for reading");
+            let mut buf = String::new();
+            file.read_to_string(&mut buf).expect("file to be readable");
+            buf
+        }
     }
 }
